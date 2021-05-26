26608414fc016778988f832f501b665e0bbd2ce4
Cole <cole.shirley@artificertech.com>
2021-05-26 09:55:54 -0600

    cleaned up belongsTo relationship code

--- src/MorphTo.php
+++ src/MorphTo.php
@@ -3,6 +3,7 @@
 namespace Artificertech\RelationshipEvents;
 
 use Artificertech\RelationshipEvents\Contracts\EventDispatcher;
+use Artificertech\RelationshipEvents\Traits\HasBelongsToEvents;
 use Artificertech\RelationshipEvents\Traits\HasEventDispatcher;
 use Illuminate\Database\Eloquent\Relations\MorphTo as MorphToBase;
 
@@ -15,54 +16,5 @@ use Illuminate\Database\Eloquent\Relations\MorphTo as MorphToBase;
 class MorphTo extends MorphToBase implements EventDispatcher
 {
     use HasEventDispatcher;
-
-    /**
-     * Associate the model instance to the given parent.
-     *
-     * @param \Illuminate\Database\Eloquent\Model|int|string $model
-     *
-     * @return \Illuminate\Database\Eloquent\Model
-     */
-    public function associate($model)
-    {
-        // If the "associating" event returns false we'll bail out of the associate and return
-        // false, indicating that the associate failed. This provides a chance for any
-        // listeners to cancel associate operations if validations fail or whatever.
-        if ($this->willDispatchEvents() && $this->child->fireModelMorphToEvent('associating', $this->eventRelationship, $model) === false) {
-            return false;
-        }
-
-        $result = parent::associate($model);
-
-        if ($result && $this->willDispatchEvents()) {
-            $this->child->fireModelMorphToEvent('associated', $this->eventRelationship, $model);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Dissociate previously associated model from the given parent.
-     *
-     * @return \Illuminate\Database\Eloquent\Model
-     */
-    public function dissociate()
-    {
-        $parent = $this->getResults();
-
-        // If the "dissociating" event returns false we'll bail out of the dissociate and return
-        // false, indicating that the dissociate failed. This provides a chance for any
-        // listeners to cancel dissociate operations if validations fail or whatever.
-        if ($this->willDispatchEvents() && $this->child->fireModelMorphToEvent('dissociating', $this->eventRelationship, $parent) === false) {
-            return false;
-        }
-
-        $result = parent::dissociate();
-
-        if (!is_null($parent) && $this->willDispatchEvents()) {
-            $this->child->fireModelMorphToEvent('dissociated', $this->eventRelationship, $parent);
-        }
-
-        return $result;
-    }
+    use HasBelongsToEvents;
 }
---
+++ src/Traits/HasBelongsToEvents.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Artificertech\RelationshipEvents\Traits;
+
+/**
+ * Trait HasOneOrManyMethods.
+ */
+trait HasBelongsToEvents
+{
+
+    /**
+     * Associate the model instance to the given parent.
+     *
+     * @param \Illuminate\Database\Eloquent\Model|int|string $model
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function associate($model)
+    {
+        // If the "associating" event returns false we'll bail out of the associate and return
+        // false, indicating that the associate failed. This provides a chance for any
+        // listeners to cancel associate operations if validations fail or whatever.
+        if ($this->willDispatchEvents() && $this->child->fireModelRelationshipEvent('associating', $this->eventRelationship, true, $this->child, $model) === false) {
+            return false;
+        }
+
+        $result = parent::associate($model);
+
+        if ($result && $this->willDispatchEvents()) {
+            $this->child->fireModelRelationshipEvent('associated', $this->eventRelationship, false, $this->child, $model);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Dissociate previously associated model from the given parent.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function dissociate()
+    {
+        $parent = $this->getResults();
+
+        // If the "dissociating" event returns false we'll bail out of the dissociate and return
+        // false, indicating that the dissociate failed. This provides a chance for any
+        // listeners to cancel dissociate operations if validations fail or whatever.
+        if ($this->willDispatchEvents() && $this->child->fireModelRelationshipEvent('dissociating', $this->eventRelationship, true, $this->child, $parent) === false) {
+            return false;
+        }
+
+        $result = parent::dissociate();
+
+        if (!is_null($parent) && $this->willDispatchEvents()) {
+            $this->child->fireModelRelationshipEvent('dissociated', $this->eventRelationship, false, $this->child, $parent);
+        }
+
+        return $result;
+    }
+}
